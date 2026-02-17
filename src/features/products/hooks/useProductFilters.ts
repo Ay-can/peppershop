a40f846f8a01e720b089113755d9720be0d0bc01
Ay-can <aycan.acn2020@gmail.com>
2026-02-17 18:03:54 +0100

    add: hook for product filters

---
+++ src/features/products/hooks/useProductFilters.ts
@@ -0,0 +1,21 @@
+import { useMemo, useState } from "react";
+import type { Product, ProductFilters } from "../types/product.types";
+import { filterProducts } from "../utils/filterProducts";
+
+export function useProductFilters(products: Product[]) {
+  const [filters, setFilters] = useState<ProductFilters>({
+    searchTerm: "",
+    maxScoville: 2_500_000,
+    selectedColor: "all",
+  });
+
+  const filteredProducts = useMemo(() => {
+    return filterProducts(products, filters);
+  }, [products, filters]);
+
+  return {
+    filters,
+    setFilters,
+    filteredProducts,
+  };
+}
