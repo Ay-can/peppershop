ce680c15cd0fbb505304e8bff63b43d197490744
Ay-can <aycan.acn2020@gmail.com>
2026-02-17 17:08:25 +0100

    add: union type and interface for products

---
+++ src/features/products/types/product.types.ts
@@ -0,0 +1,25 @@
+export type ProductCategory =
+  | "Kruiden en specerijen"
+  | "Sauzen en dressings"
+  | "Pittige snacks"
+  | "Exotische pepers"
+  | "Chilisaus";
+
+export type ProductColor = "rood" | "oranje" | "groen" | "geel";
+
+export interface Product {
+  id: string;
+  category: ProductCategory;
+  name: string;
+  description: string;
+  price: number;
+  scoville: number;
+  color: ProductColor;
+  image: string;
+}
+
+export interface ProductFilters {
+  searchTerm: string;
+  maxScoville: number;
+  selectedColor: ProductColor | "all";
+}
